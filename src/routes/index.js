654b1f47d2c27cef88303f003b7675834f9efbde
Bishuu1 <tlz98x@gmail.com>
2020-06-12 22:49:28 -0400

    ahora si

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const items =[
-    {id:1,name:'Curriculum 1'},
-    {id:2,name:'Curriculum 2'},
-    {id:3,name:'Curriculum 3'}
-];
-
 router.get('/',(req,res)=> {
 	res.render('index', {
         title:'CV Generator'
