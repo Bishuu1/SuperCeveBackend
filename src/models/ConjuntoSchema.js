96de177df4b3e7632f1f80168b6e2c1869467d19
daniela <daniela.moreno1@mail.udp.cl>
2020-06-27 19:54:34 -0400

    Backend de setEntries y sus rutas

---
+++ src/models/ConjuntoSchema.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+
+const ConjuntoSchema = new Schema ({
+    NombreConjuntoEntradas: {type:String},
+    Descripcion: {type:String},
+    Entradas: [{type:String}],
+    Usuario: {//CAMPO REQUERIDO PARA LAS SESIONES EN TODOS LOS ESQUEMAS MENOS EN USUARIOS
+        type: String
+    }
+}, {
+    timestamps:true
+});
+
+module.exports = model('Conjunto', ConjuntoSchema);
+
--- src/routes/SetEntries.js
+++ src/routes/SetEntries.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 const router = Router();
-const { getSetEntries , getSetEntry , createSetEntries,deleteSetEntries,updateSetEntries} = require('../controllers/SetEntriesController');
+const { getEntries , getEntry , createEntries,deleteEntries,updateEntries,
+    getSetEntries , getSetEntry , createSetEntries,deleteSetEntries,
+    updateSetEntries} = require('../controllers/SetEntriesController');
 
-router.route('/')
+router.route('/Entry')
+    .get(getEntries)//muestra todo
+    .post(createEntries)//trae formulario
+router.route('/SetEntry') 
     .get(getSetEntries)//muestra todo
-    .post(createSetEntries)//trae formulario
-    
-router.route('/:EntradaId')
+    .post(createSetEntries)//trae formulario 
+router.route('/Entry/:EntradaId')
+    .get(getEntry)//muestra uno
+    .put(updateEntries)//actualiza
+    .delete(deleteEntries)//elimina
+router.route('/SetEntry/:ConjuntoId')    
     .get(getSetEntry)//muestra uno
     .put(updateSetEntries)//actualiza
     .delete(deleteSetEntries)//elimina
